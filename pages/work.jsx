50dfc396b6eda1e0af76e71342200e908dc13a5e
Ghaith-13 <ghaethoo2001.go@gmail.com>
2023-09-12 04:53:02 -0400

    update the code

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Portofolio from '../components/portofolio'
+import React from 'react';
+import Hero from '../components/Hero';
 
 function work() {
   return (
     <div>
-        <Hero heading="My Work" message="this is some of my work." />
-        <Portofolio />
+      <Hero heading="My Work" message="this is some of my work." />
+      <Portofolio />
     </div>
-  )
+  );
 }
 
-export default work
+export default work;
